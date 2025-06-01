e5b9abec73bc56c7275f2006a16649079f06825f
devdev29 <devanshjoshi39@gmail.com>
2025-06-01 14:22:18 +0530

    fixed create_server code

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+import os
 
 
 SERVER_ADDR = ("127.0.0.1", 8000)
@@ -8,15 +9,21 @@ def main():
     print("Logs from your program will appear here!")
     
     # Uncomment this to pass the first stage
-    server_socket = socket.create_server(address=SERVER_ADDR, reuse_port=True)
+    server_socket = create_server(address=SERVER_ADDR, reuse_port=True)
     server_socket.accept() # wait for client
     server_socket.send("HTTP/1.1 200 OK\r\n\r\n")
 
 
 def create_server(address: tuple, reuse_port: bool = False, backlog: int|None = None):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(SERVER_ADDR)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+    server_socket.bind(address)
+    if os.name not in ('nt', 'cygwin'):
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if reuse_port:
+        try:
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except:
+            raise ValueError("SO_REUSEPORT not supported on this platform")
     if backlog:
         server_socket.listen(backlog)
     else:
